e8c6cf4fdb08723e7afc79d98a3e34ead4caaa6b
Abdelhamid Fadel <fadelabdelhamid1@gmail.com>
2023-12-29 01:58:34 +0100

    Added descripton for NotValidProjectException

--- Fadel Abdelhamid - UML Diagrams Generator/src/org/mql/java/umlgen/exceptions/NotValidProjectException.java
+++ Fadel Abdelhamid - UML Diagrams Generator/src/org/mql/java/umlgen/exceptions/NotValidProjectException.java
@@ -1,5 +1,15 @@
 package org.mql.java.umlgen.exceptions;
 
+/**
+ * Exception thrown when the given path is not a valid Java Project.
+ * Including but not limited to the following reasons:
+ * <ul>
+ * <li>Path is malformed</li>
+ * <li>Path does not exist</li>
+ * <li>Path is not a directory</li>
+ * <li>Directory does not have .class files including its subfolders</li>
+ * </ul>
+ */
 public class NotValidProjectException extends Exception {
 
 	private static final long serialVersionUID = 1L;
