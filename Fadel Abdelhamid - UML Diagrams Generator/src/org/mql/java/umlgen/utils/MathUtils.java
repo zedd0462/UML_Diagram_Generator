93e3a1a8a9cbaf180a733de309f89f4a1497d992
Abdelhamid Fadel <fadelabdelhamid1@gmail.com>
2024-01-23 17:42:33 +0100

    Added some utils.

---
+++ Fadel Abdelhamid - UML Diagrams Generator/src/org/mql/java/umlgen/utils/MathUtils.java
@@ -0,0 +1,25 @@
+package org.mql.java.umlgen.utils;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class MathUtils {
+
+	private MathUtils() {}
+	
+	public static int sum(Collection<Integer> list) {
+	     int sum = 0; 
+
+	     for (int i : list)
+	         sum = sum + i;
+
+	     return sum;
+	}
+	
+	public static int max(Collection<? extends Integer> coll) {
+		return Collections.max(coll);
+	}
+	
+	
+
+}
